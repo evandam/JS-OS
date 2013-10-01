ac334af9eb6fa1460c64b6d7bfa03dedebef2795
evandam <ecv92@yahoo.com>
2013-09-30 20:06:59 -0400

    CPU display updates every cycle

--- scripts/host/cpu.js
+++ scripts/host/cpu.js
@@ -32,7 +32,11 @@ function Cpu() {
     
     this.cycle = function() {
         krnTrace("CPU cycle");
+
         // TODO: Accumulate CPU usage and profiling statistics here.
         // Do the real work here. Be sure to set this.isExecuting appropriately.
+
+
+        updateCPUDisplay();
     };
 }
