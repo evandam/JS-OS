084bf329b11266e71de7127d7835f786bbda3035
evandam <ecv92@yahoo.com>
2013-10-03 19:51:05 -0400

    merge conflicts resolved..back to normal

--- scripts/os/mmu.js
+++ scripts/os/mmu.js
@@ -5,6 +5,18 @@ and later ensure that each program is within its own memory space,
 and also perform context switching.
 */
 
+function MMU() {
+    
+}
+
+MMU.prototype.read = function (pid, addr) {
+    return _Memory.mem[addr];   // check base/limit here
+};
+
+MMU.prototype.write = function (pid, addr, toWrite) {
+    _Memory.mem[addr] = toWrite;
+}
+
 var MemoryManagementUnit = function () {
     this.PCBs = [];
     // TODO: memory access function for PCB with permission checks
