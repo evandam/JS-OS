657cccb63feaa593cf6ace4b0f8b6c64ac0a2921
evandam <ecv92@yahoo.com>
2013-11-18 13:40:06 -0500

    Create file working, think about null-terminating?

--- scripts/os/deviceDriverFileSystem.js
+++ scripts/os/deviceDriverFileSystem.js
@@ -31,16 +31,26 @@ DeviceDriverFileSystem.prototype.krnFileSystemIO = function (params) {
 };
 
 DeviceDriverFileSystem.prototype.create = function (filename) {
-    console.log('create ' + filename);
     var dirEntry = this.getNextDirEntry();
     var fileEntry = this.getNextFileEntry();
-    var dirData = '1' + fileEntry + filename;
+    var dirData = '1' + fileEntry + filename.toUpperCase();       // available byte, address of file data, then the filename
     var remainingBytes = BLOCK_SIZE - dirData.length;
-    for (var i = 0; i < remainingBytes; i++)
+    for (var i = 0; i < remainingBytes; i++)    // pad with 0s
         dirData += '0';
     localStorage.setItem(dirEntry, dirData);
     updateFileSystemDisplay(dirEntry, dirData);
-    console.log(dirData);
+    this.updateNextDirEntry();  // update the MBR now that this addr is taken
+
+    var fileData = localStorage.getItem(fileEntry);
+    fileData = '1' + fileData.substring(1);  // set to taken
+    localStorage.setItem(fileEntry, fileData);
+    updateFileSystemDisplay(fileEntry, fileData);
+    this.updateNextFileEntry(); // update the MBR to point to next avail file addr
+    this.incrementSize();   // another block is taken up, so update that in the mbr too (not sure if this will be used)
+
+    // update MBR display
+    updateFileSystemDisplay('000', localStorage.getItem('000'));
+    
 };
 
 DeviceDriverFileSystem.prototype.read = function (filename) {
@@ -67,6 +77,7 @@ DeviceDriverFileSystem.prototype.format = function () {
             for (var block = 0; block < BLOCKS; block++) {
                 var tsb = track + '' + sector + '' + block;
                 localStorage.setItem(tsb, formattedVal);
+                updateFileSystemDisplay(tsb, formattedVal);
             }
         }
     }
@@ -77,7 +88,7 @@ DeviceDriverFileSystem.prototype.format = function () {
     var mbrData = '0011000';     
     localStorage.setItem('000', mbrData);
 
-    initFileSystemDisplay();
+    updateFileSystemDisplay('000', mbrData);
 
     _StdIn.putText('format complete!');
     _StdIn.advanceLine();
@@ -85,11 +96,84 @@ DeviceDriverFileSystem.prototype.format = function () {
     // not sure what would cause an error just yet...
 };
 
-// just read it from the mbr
+// bytes [0:3] in MBR = available dir entry
 DeviceDriverFileSystem.prototype.getNextDirEntry = function () {
     return localStorage.getItem('000').substring(0, 3);
 };
 
+// bytes [3:6] in MBR = available file entry
 DeviceDriverFileSystem.prototype.getNextFileEntry = function () {
     return localStorage.getItem('000').substring(3, 6);
 };
+
+// bytes [6:9] in MBR = number of blocks taken (why not)
+DeviceDriverFileSystem.prototype.getAllocateSize = function () {
+    return parseInt(localStorage.getItem('000').substring(6,9));
+};
+
+// Get the next available directory entry and update the MBR with it
+// Will usually be the next addr, but may not be if wrapping around
+DeviceDriverFileSystem.prototype.updateNextDirEntry = function () {
+    var addr = this.getNextDirEntry();
+    var startAddr = addr;
+    // probing for next entry that is not taken (first byte=available)
+    while (localStorage.getItem(addr)[0] != '0') {
+        // increment the block
+        addr = addr[0] + addr[1] + (parseInt(addr[2]) + 1);
+        // check the one's place for overflow
+        if (parseInt(addr[2]) === BLOCKS) {
+            addr = addr[0] + (parseInt(addr[1]) + 1) + '0';
+            // wrap around to block 001 once limit reached
+            if (parseInt(addr[1]) === SECTORS) {
+                addr = '001';
+            }
+        }
+        if (addr == startAddr) {
+            krnTrapError('NO AVAILABLE DIRECTORY ENTRIES!');
+            return;
+        }
+    }
+    var mbrData = localStorage.getItem('000');
+    mbrData = addr + mbrData.substring(3);
+    localStorage.setItem('000', mbrData);
+};
+
+DeviceDriverFileSystem.prototype.updateNextFileEntry = function () {
+    var addr = this.getNextFileEntry();
+    var startAddr = addr;
+    // probing for next entry that is not taken (first byte=available)
+    while (localStorage.getItem(addr)[0] != '0') {
+        // increment the block
+        addr = addr[0] + addr[1] + (parseInt(addr[2]) + 1);
+        // check the one's place for overflow (blocks)
+        if (parseInt(addr[2]) === BLOCKS) {
+            addr = addr[0] + (parseInt(addr[1]) + 1) + '0';
+            // check ten's for overflow (sectors)
+            if (parseInt(addr[1]) === SECTORS) {
+                addr = (parseInt(addr[0]) + 1) + '0' + '0';
+                // finally check hundreds for overflow (tracks) and wrap around
+                if (parseInt(addr[0]) === TRACKS) {
+                    addr = '100';
+                }
+            }
+        }
+        if (addr == startAddr) {
+            krnTrapError('NO AVAILABLE DIRECTORY ENTRIES!');
+            return;
+        }
+    }
+    console.log(addr + ' = next file addr');
+    var mbrData = localStorage.getItem('000');
+    mbrData = mbrData.substring(0,3) + addr + mbrData.substring(6);
+    localStorage.setItem('000', mbrData);
+};
+
+// Can use the remaining bytes [6:] of the MBR to track the number of blocks taken
+// Useful for eventually checking if theres sufficient space to create/write
+// And possibly query it in the 'ls' command
+DeviceDriverFileSystem.prototype.incrementSize = function () {
+    var mbrData = localStorage.getItem('000');
+    var size = parseInt(mbrData.substring(6));
+    size++;
+    localStorage.setItem('000', mbrData.substring(0, 6) + size);
+}
